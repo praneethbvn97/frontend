5bacba661f3c00ff5b76962469b788b9c5325c14
praneethbvn97 <62839600+praneethbvn97@users.noreply.github.com>
2020-06-17 09:41:40 +1000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL:"https://backcamp.herokuapp.com/universityApi/",
+  apiURL:"https://campback.herokuapp.com/universityApi/",
   facultySecurityCode:'true_faculty',
   mApiKey:'AIzaSyCDKBu1aPoiFQX0tCZUJJ2I8_JRW7f_vmU'
 };
